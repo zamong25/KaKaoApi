c113b15d89cab3f84c6a7cd446ef7311bef1656b
zamong25 <wool0602@naver.com>
2021-05-09 18:06:05 +0900

    Initial Commit

---
+++ KaKaoApi/KaKaoApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaKaoApiApp.swift
+//  KaKaoApi
+//
+//  Created by PCW on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct KaKaoApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
